f190b2e1a4fd77bdfb00b7426825952e824ad59c
Nyan Lin Maung <nyanlinmg2666@gmail.com>
2026-04-13 17:42:34 +0630

    fix: remove submodule properly

---
+++ first/src/App.js
@@ -0,0 +1,58 @@
+import {useRef, useState} from 'react';
+
+function Item(props) {
+  return ( 
+    <li>{props.name} , $ {props.price}</li>
+  )
+}
+
+function AddForm(props) {
+    const nameRef = useRef();
+    const priceRef = useRef();
+
+    return (
+        <form onSubmit={ e => {
+            e.preventDefault();
+
+            props.add(
+                nameRef.current.value,
+                priceRef.current.value
+            );
+
+        } }>
+
+            <input type='text' ref={nameRef}></input> <br />
+            <input type='text' ref={priceRef}></input> <br />
+            <button type='submit'>Add</button>
+
+        </form>
+    )
+}
+
+export default function App() {
+    const [data, setData] = useState([
+        {id : 1, name : "Apple", price : 0.99},
+        {id : 2, name : "Orange", price : 0.88}
+    ]);
+
+    const add = (name, price) => {
+        const id = data.length + 1;
+        setData([
+            ...data,
+            {id, name , price}
+        ]);
+    }
+
+    return (
+        <div>
+            <h1>Hello React</h1>
+            <ul>
+              {data.map( i => (
+                <Item key={i.id} name={i.name} price={i.price}></Item>
+              ))}
+          </ul>
+
+          <AddForm add={add} />
+        </div>
+    )
+}
